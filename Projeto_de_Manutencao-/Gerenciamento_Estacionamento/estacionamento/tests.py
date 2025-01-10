d163e7f407917c892ef17a5c9c7a7581d252ccd1
MichelyFelix <michelyvitoriauzl@gmail.com>
2025-01-10 18:09:52 -0300

    ajustes nos testes

--- Projeto_de_Manutencao-/Gerenciamento_Estacionamento/estacionamento/tests.py
+++ Projeto_de_Manutencao-/Gerenciamento_Estacionamento/estacionamento/tests.py
@@ -3,116 +3,123 @@ from django.utils import timezone
 from rest_framework import status
 from estacionamento.models import Cliente, Veiculo, Estacionamento
 
+
 class ClienteTesteCase(TestCase):
-
     def setUp(self):
-
-        self.cliente = Cliente.objects.create(
+        self.novo_cliente = Cliente.objects.create(
             nome="Marta Silva",
-            cpf="12345678912",
+            CPF="12345678912",
             telefone=991234567
         )
-    def test_cadastrar_cliente(self):
 
+    def test_cadastrar_cliente(self):
         url = "http://localhost:8000/clientes"
         data = {
             "nome": "Claudio Maia",
-            "cpf": "12345671245",
+            "CPF": "12345671245",
             "telefone": 997654321
-            }
+        }
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTPS_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Cliente.objects.filter(nome="Claudio Maia").exists())
 
-        response = self.client.post(url,data)
-        self.assertEqual(response.status_code,status.HTTP_409_CONFLICT)
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
 
     def test_listar_cliente(self):
         url = "http://localhost:8000/clientes"
         Cliente.objects.create(
             nome="Claudio Maia",
-            cpf="12345671245",
-            telefone= 997654321
-            )
-
+            CPF="12345671245",
+            telefone=997654321
+        )
         response = self.client.get(url)
-        self.assertEqual(response.status_code,status.HTTPS_200_OK)
-        self.assertEqual(response.data[0]['nome'],3)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_atualizar_cliente(self):
         url = f"http://localhost:8000/clientes/{self.novo_cliente.id}/"
         data = {
             "nome": "Maria Maia",
-            "cpf": "12348671245",
+            "CPF": "12348671245",
             "telefone": 997655321
-                }
-
+        }
         response = self.client.put(url, data)
-        self.assertEqual(response.status_code, status>HTTPS_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        def teste_delet_cliente(self):
-            url = f"http://localhost:8000/clientes/{self.novo_cliente.id}/"
-            response = self.client.delete(url)
-            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-            self.assertFalse(Cliente.objects.filter(id=self.novo_cliente.id).exists())
+    def test_deletar_cliente(self):
+        url = f"http://localhost:8000/clientes/{self.novo_cliente.id}/"
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Cliente.objects.filter(id=novo_cliente.id).exists())
 
-class veiculoTesteCase(TestCase):
+
+class VeiculoTesteCase(TestCase):
+
     def setUp(self):
-        self.cliente = Cliente.objects.create(
+        self.novo_cliente = Cliente.objects.create(
             nome="Marta Silva",
             CPF="12345678912",
             telefone=991234567
         )
-        self.veiculo = Veiculo.objects.create(
-            cliente=self.cliente,
+        self.novo_veiculo = Veiculo.objects.create(
+            cliente=self.novo_cliente,
             modelo="Toyota Corolla",
             placa="ABC1234",
             cor="Preto",
             tipo="Carro"
         )
+
     def test_cadastrar_veiculo(self):
         url = "http://localhost:8000/veiculos"
         data = {
-            "cliente": self.cliente.id,
+            "cliente": self.novo_cliente.id,
             "modelo": "Toyota Corolla",
             "placa": "ABC1244",
             "cor": "Cinza",
             "tipo": "Carro"
         }
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTPS_201_CREATED)
-        self.assertTrue(Cliente.objects.filter(placa="ABC1244").exists())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Veiculo.objects.filter(placa="ABC1244").exists())
 
-        response = self.client.post(url,data)
-        self.assertEqual(response.status_code,status.HTTP_409_CONFLICT)
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
 
     def test_listar_veiculos(self):
         url = "http://localhost:8000/veiculos"
+        Veiculo.objects.create(
+            cliente=self.novo_cliente,
+            modelo="Toyota Corolla",
+            placa="ABC1234",
+            cor="Preto",
+            tipo="Carro"
+        )
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data[0]['placa'], "ABC1234")
-    
+
     def test_atualizar_veiculo(self):
         url = f"http://localhost:8000/veiculos/{self.novo_veiculo.id}/"
         data = {
-            "cliente" :self.cliente,
-            "modelo" :"Toyota Corolla" 
-            "placa" : "ABC1244"
-            "cor" : "cinza" 
-            "tipo" : "carro"
-                }
-
+            "cliente": self.novo_cliente.id,
+            "modelo": "Toyota Corolla",
+            "placa": "ABC2244",
+            "cor": "Cinza",
+            "tipo": "Carro"
+        }
         response = self.client.put(url, data)
-        self.assertEqual(response.status_code, status>HTTPS_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def teste_delet_veiculo(self):
-            url = f"http://localhost:8000/veiculos/{self.novo_veiculo.id}/"
-            response = self.client.delete(url)
-            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-            self.assertFalse(Cliente.objects.filter(id=self.novo_veiculo.id).exists())
+    def test_deletar_veiculo(self):
+        url = f"http://localhost:8000/veiculos/{self.novo_veiculo.id}/"
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Veiculo.objects.filter(id=self.novo_veiculo.id).exists())
 
-class EstacionamentoTestecase(TestCase):
-     def setUp(self):
+
+class EstacionamentoTesteCase(TestCase):
+
+    def setUp(self):
         self.cliente = Cliente.objects.create(
             nome="Marta Silva",
             CPF="12345678912",
@@ -132,8 +139,8 @@ class EstacionamentoTestecase(TestCase):
         )
 
     def test_criar_estacionamento(self):
-        url = "http://localhost:8000/veiculos"
-            data = {
+        url = "http://localhost:8000/estacionamentos"
+        data = {
             "cliente": self.cliente.id,
             "veiculos": self.veiculo.id,
             "horario_entrada": timezone.now()
@@ -141,5 +148,3 @@ class EstacionamentoTestecase(TestCase):
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Estacionamento.objects.filter(cliente=self.cliente).exists())
-
-    
